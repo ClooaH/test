58cd9466c792fb05b28b57f3dc0e7645d0eec040
ClooaH <adam.nilsson@elev.ga.ntig.se>
2018-11-22 10:33:06 +0100

    Vury naice

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+        JOptionPane.showMessageDialog(null, "Heyo Magnus");
+
     }
 }
